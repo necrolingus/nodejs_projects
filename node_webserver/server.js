2c081f40ecd1d784b39eaa962c124297dbcc3361
necrolingus <leigh.williams88@gmail.com>
2018-09-01 17:34:16 +0200

    done

--- node_webserver/server.js
+++ node_webserver/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
 var app = express();
 
 
@@ -10,14 +11,58 @@ var app = express();
 //is located
 hbs.registerPartials(__dirname+'/views/partials');
 app.set('view engine', 'hbs');
-app.use(express.static(__dirname + '/public'));
+
 //app.use calls up middleware. In this case built in middleware
 //__dirname stores the path to your poject
 //now we can call anything in the public folder
 
-hbs.registerHelper('getCurrentYear',()=>{
-  return new Date().getFullYear()
+
+function serverLogger (req, callback) {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+  console.log(log);
+
+  fs.appendFile('server.log', log + '\r\n', (err) => { //err can be called anything
+    if (err) {
+      callback('An error occurred');
+      //console.log('Unable to write to file');
+    }
+  });
+};
+
+//express.use is called in the order they are in the file!!!!!
+
+//check here for all the functions available on express req and res
+//https://expressjs.com/en/4x/api.html#req
+app.use((req, res, next) => {
+  serverLogger(req, (errorMsg) => {
+    if (errorMsg){
+      console.log(`Could not write to file: ${errorMsg}`);
+    }
+  });
+  next();
+
 });
+
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs',{
+//     maintenanceMessage: 'We are currenlty down for maintenance'
+//   });
+//   //without next we wont be moving on
+//   //next();
+// });
+
+app.use(express.static(__dirname + '/public'));
+
+
+hbs.registerHelper('getCurrentYear',()=>{
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt',(textToScream)=>{
+  return textToScream.toUpperCase();
+});
+
 
 app.get('/',(req, res)=>{
     res.render('home.hbs', { //this object is how you inject values
